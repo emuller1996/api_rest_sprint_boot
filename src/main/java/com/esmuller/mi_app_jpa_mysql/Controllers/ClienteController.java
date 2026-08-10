072227674b4159b611266f925df7e1a46c23e3fc
emuller1996 <stefano.muller@hotmail.com>
2026-08-10 18:12:20 -0500

    feat : se agrega end point para modificar cliente y obtener

--- src/main/java/com/esmuller/mi_app_jpa_mysql/Controllers/ClienteController.java
+++ src/main/java/com/esmuller/mi_app_jpa_mysql/Controllers/ClienteController.java
@@ -5,18 +5,24 @@ import com.esmuller.mi_app_jpa_mysql.services.CategoriaService;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.esmuller.mi_app_jpa_mysql.dtos.ClienteDTO;
+import com.esmuller.mi_app_jpa_mysql.dtos.CrearProductoRequest;
+import com.esmuller.mi_app_jpa_mysql.dtos.ProductoDTO;
 import com.esmuller.mi_app_jpa_mysql.services.ClienteService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -34,6 +40,25 @@ public class ClienteController {
     @GetMapping
     public List<ClienteDTO> getAllClientes() {
         return clienteService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteDTO> getCliente(@PathVariable Long id) {
+        return clienteService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    // Actualizar cliente
+    @PutMapping("/{id}")
+    public Map<String, Object> updateCliente(@PathVariable Long id, 
+                                                      @RequestBody ClienteDTO request) {
+        Optional<ClienteDTO> clienteActualizado = clienteService.update(id, request);
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Cliente actualizado exitosamente");
+        response.put("data", clienteActualizado);
+        return response;
     }
 
     @PostMapping
--- src/main/java/com/esmuller/mi_app_jpa_mysql/services/ClienteService.java
+++ src/main/java/com/esmuller/mi_app_jpa_mysql/services/ClienteService.java
@@ -1,6 +1,7 @@
 package com.esmuller.mi_app_jpa_mysql.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.esmuller.mi_app_jpa_mysql.dtos.CategoriaDTO;
 import com.esmuller.mi_app_jpa_mysql.dtos.ClienteDTO;
+import com.esmuller.mi_app_jpa_mysql.dtos.CrearProductoRequest;
+import com.esmuller.mi_app_jpa_mysql.dtos.ProductoDTO;
 import com.esmuller.mi_app_jpa_mysql.entities.Categoria;
 import com.esmuller.mi_app_jpa_mysql.entities.Cliente;
+import com.esmuller.mi_app_jpa_mysql.entities.Producto;
 import com.esmuller.mi_app_jpa_mysql.repositories.ClienteRepository;
 
 @Service
@@ -26,6 +30,12 @@ public class ClienteService {
                 .collect(Collectors.toList());
     }
 
+    // Obtener cliente por ID
+    public Optional<ClienteDTO> findById(Long id) {
+        return clienteRepository.findById(id)
+                .map(this::convertToDTO);
+    }
+
     @Transactional
     public ClienteDTO create(ClienteDTO dto) {
 
@@ -37,6 +47,21 @@ public class ClienteService {
         Cliente saved = clienteRepository.save(cliente);
         return convertToDTO(saved);
     }
+
+    @Transactional
+    public Optional<ClienteDTO> update(Long id, ClienteDTO request) {
+        return clienteRepository.findById(id)
+                .map(cliente -> {
+                    // Actualizar campos básicos
+                    cliente.setNombre(request.getNombre());
+                    cliente.setDireccionn(request.getDireccion());
+                    cliente.setTelefono(request.getTelefono());
+                    Cliente updated = clienteRepository.save(cliente);
+                    return convertToDTO(updated);
+                });
+    }
+
+    
 
     private ClienteDTO convertToDTO(Cliente cliente) {
         ClienteDTO dto = new ClienteDTO();
